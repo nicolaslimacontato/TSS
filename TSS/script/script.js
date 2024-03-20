f9b8642c3701d4ec2dbefd036bcd488db87c048f
Nicolas Lima <nicolaslimacontato@outlook.com>
2024-03-20 13:37:38 -0300

    Add files via upload

--- TSS/script/script.js
+++ TSS/script/script.js
@@ -1,10 +1,24 @@
+//menu
 const navEl = document.querySelector('.navbar');
 
 window.addEventListener('scroll', function (){
-    if(window.scrollY > 400) {
+    if(window.scrollY > 300) {
         navEl.classList.add('navbar-scrolled');
     } else {
         navEl.classList.remove('navbar-scrolled');
     }
 });
 
+// init Isotope
+var $grid = $('#product-list').isotope({});
+        
+// filter items on button click
+$('.filter-button-group').on('click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+});
+
+// AOS
+AOS.init({
+    duration: 1800,
+});
